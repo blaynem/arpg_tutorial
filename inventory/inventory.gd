0606a74a75bb3e7ed8503f0a18e808c3c461b50b
Blayne Marjama <blayne.marjama@gmail.com>
2024-02-06 22:01:43 -0800

    players now have an inventory that makes more sense! stacking, and limiting items

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -4,11 +4,27 @@ class_name Inventory
 
 signal updated
 
-@export var items: Array[InventoryItem]
+@export var slots: Array[InventorySlot]
 
-func insert(item: InventoryItem):
-	for i in range(items.size()):
-		if !items[i]:
-			items[i] = item
-			break
+
+func insert(newItem: InventoryItem):
+	# Find slots that already contain the same item as the newItem, and can also hold more of it.
+	var slotsWithItem = slots.filter(func(slot): return slot.item == newItem && slot.amount < newItem.maxStackSize)
+	# All slots that are empty.
+	var emptySlots = slots.filter(func(slot): return slot.item == null)
+
+	# If there are no slots that can hold more of the newItem, and there are no empty slots we need to error out.
+	if slotsWithItem.is_empty() and emptySlots.is_empty():
+		print("Inventory is full")
+		return
+	
+	# Because we filtered the slots array, we can be sure that the first element of the array is the first slot that can hold more of the newItem.
+	if not slotsWithItem.is_empty():
+		slotsWithItem[0].amount += 1
+	# And if there are no slots with the newItem, we can be sure that the first element of the emptySlots array is the first empty slot.
+	else:
+		emptySlots[0].item = newItem
+		emptySlots[0].amount = 1
+	
+	# Emit an 'updated' signal to notify other components of the inventory change.
 	updated.emit()
