bd6b8ac3bc8e92570ee28384c0f774a82ecfe002
Blayne Marjama <blayne.marjama@gmail.com>
2024-02-10 14:08:25 -0800

    projectiles successfully die on walls now

--- resources/overlap/ProjectileHitBox.gd
+++ resources/overlap/ProjectileHitBox.gd
@@ -50,3 +50,9 @@ func _physics_process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	pass
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Terrain"):
+		destroy()
+	pass # Replace with function body.
